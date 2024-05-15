9350ff86d70d7ebc6b046ab41fd39bdb81851462
SrDavidC <david.cerdasalvarado@ucr.ac.cr>
2024-05-14 22:06:27 -0600

    feat: added view for index.html

--- djangoProject/views.py
+++ djangoProject/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render, HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'index.html')
