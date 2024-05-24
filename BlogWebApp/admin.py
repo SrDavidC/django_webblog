682c79c386b262d180500950a5ea5f6568c06628
SrDavidC <david.cerdasalvarado@ucr.ac.cr>
2024-05-23 18:17:53 -0600

    feat: added Model for BlogPost

--- BlogWebApp/admin.py
+++ BlogWebApp/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Message
+from .models import BlogPost
 
 admin.site.register(Message)
+
+admin.site.register(BlogPost)
