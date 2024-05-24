89f01cd6a8c3c5f97f34b27a22f5d42abc7e02e6
SrDavidC <david.cerdasalvarado@ucr.ac.cr>
2024-05-23 18:20:37 -0600

    feat: added sample data for web blog posts

--- BlogWebApp/models.py
+++ BlogWebApp/models.py
@@ -9,3 +9,12 @@ class Message(models.Model):
     def is_valid(self):
         pass
     # TO DO: check if it is necessary
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=50)
+    thumbnail_url = models.URLField()
+    preview_text = models.TextField()
+    readingTimeInMinutes = models.IntegerField()
+    categories = models.TextField() # Could be text field separated by comma: academic, biological, vacations or with foreign key.
+    publicationDate = models.DateField
+
